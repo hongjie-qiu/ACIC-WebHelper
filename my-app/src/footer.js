9fa34adc3ca4f8cc8e4a254dbc8adc6b703e0720
Jeffrey Qiu <hongjie.qiu1@gmail.com>
2022-12-11 22:11:22 -0800

    added footer

---
+++ my-app/src/footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                &copy; 2022 University of Washington All rights reserved. Contact with 
+                <a href="mailto:jscholl@uw.edu" aria-label="Professor Hans Jochen Scholl Email"> Professor Hans Jochen Scholl</a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
